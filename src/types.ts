5bb26827faaccd939e75895be347126ea56816a9
Adam Jones <jones_adam@rocketmail.com>
2020-10-01 18:04:48 +0100

    Improve timesheet hour pre-processing

--- src/types.ts
+++ src/types.ts
@@ -105,13 +105,13 @@ export type CVResponse = {
 };
 
 export type TimesheetHours = {
-  monday: Array<WorkPeriod>;
-  tuesday: Array<WorkPeriod>;
-  wednesday: Array<WorkPeriod>;
-  thursday: Array<WorkPeriod>;
-  friday: Array<WorkPeriod>;
-  saturday: Array<WorkPeriod>;
-  sunday: Array<WorkPeriod>;
+  monday?: WorkPeriod[];
+  tuesday?: WorkPeriod[];
+  wednesday?: WorkPeriod[];
+  thursday?: WorkPeriod[];
+  friday?: WorkPeriod[];
+  saturday?: WorkPeriod[];
+  sunday?: WorkPeriod[];
 };
 
 export type WorkPeriod = {
@@ -120,6 +120,7 @@ export type WorkPeriod = {
 };
 
 export type HourAndMinute = {
-  hour: number;
-  minute: number;
+  // prettier-ignore
+  hour: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23;
+  minute: 0 | 15 | 30 | 45;
 };
